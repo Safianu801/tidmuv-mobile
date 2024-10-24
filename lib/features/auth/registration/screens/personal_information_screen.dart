d8c2fcd0e4f1f05322ecd5d7821565298b5cfa22
Nova Technology ltd <safianusani2004@gmail.com>
2024-10-24 21:12:34 +0100

    code update

--- lib/features/auth/registration/screens/personal_information_screen.dart
+++ lib/features/auth/registration/screens/personal_information_screen.dart
@@ -9,7 +9,8 @@ class PersonalInformationScreen extends StatefulWidget {
   const PersonalInformationScreen({super.key});
 
   @override
-  State<PersonalInformationScreen> createState() => _PersonalInformationScreenState();
+  State<PersonalInformationScreen> createState() =>
+      _PersonalInformationScreenState();
 }
 
 class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
@@ -19,20 +20,41 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
   final phoneNumberController = TextEditingController();
   final emailController = TextEditingController();
   AuthService authService = AuthService();
-  Future<void> collectUserInformation(BuildContext context,
-      String firstName,
-      String lastName,
-      String middleName,
-      String phoneNumber,
-      String dateofbirth,
-      String email,
-      String gender,) async {
+
+  Future<void> collectUserInformation(
+    BuildContext context,
+    String firstName,
+    String lastName,
+    String middleName,
+    String phoneNumber,
+    String dateofbirth,
+    String email,
+    String gender,
+  ) async {
     try {
-      authService.registerUser(context, firstName, lastName, middleName, phoneNumber, dateofbirth, email, gender,);
+      authService.registerUser(
+        context,
+        firstName,
+        lastName,
+        middleName,
+        phoneNumber,
+        dateofbirth,
+        email,
+        gender,
+      );
     } catch (e) {
       print(e);
     }
   }
+
+  Future<void> uploadProfile(BuildContext context) async {
+    try {
+      await authService.uploadImage(context);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,33 +73,65 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
                     height: 100,
                     width: 100,
                     decoration: BoxDecoration(
-                      color: Colors.grey[400],
-                      shape: BoxShape.circle
-                    ),
+                        color: Colors.grey[400], shape: BoxShape.circle),
                   ),
                   const Text(
                     "Upload Profile",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500
-                    ),
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   )
                 ],
               ),
-              const SizedBox(height: 20,),
-              CustomTextField(hintText: "First Name", controller: firstNameController, isObscure: false),
-              const SizedBox(height: 15,),
-              CustomTextField(hintText: "Middle Name", controller: middleNameController, isObscure: false),
-              const SizedBox(height: 15,),
-              CustomTextField(hintText: "Last Name", controller: lastNameController, isObscure: false),
-              const SizedBox(height: 15,),
-              CustomTextField(hintText: "0800-000-0000", controller: phoneNumberController, isObscure: false),
-              const SizedBox(height: 15,),
-              CustomTextField(hintText: "Email", controller: emailController, isObscure: false),
-              const SizedBox(height: 30,),
-              CustomButtonOne(title: "Proceed", onClick: (){
-                collectUserInformation(context, firstNameController.text.trim(), lastNameController.text.trim(), middleNameController.text.trim(), phoneNumberController.text.trim(), "", emailController.text.trim(), "");
-              })
+              const SizedBox(
+                height: 20,
+              ),
+              CustomTextField(
+                  hintText: "First Name",
+                  controller: firstNameController,
+                  isObscure: false),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextField(
+                  hintText: "Middle Name",
+                  controller: middleNameController,
+                  isObscure: false),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextField(
+                  hintText: "Last Name",
+                  controller: lastNameController,
+                  isObscure: false),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextField(
+                  hintText: "0800-000-0000",
+                  controller: phoneNumberController,
+                  isObscure: false),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomTextField(
+                  hintText: "Email",
+                  controller: emailController,
+                  isObscure: false),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomButtonOne(
+                  title: "Proceed",
+                  onClick: () {
+                    collectUserInformation(
+                        context,
+                        firstNameController.text.trim(),
+                        lastNameController.text.trim(),
+                        middleNameController.text.trim(),
+                        phoneNumberController.text.trim(),
+                        "",
+                        emailController.text.trim(),
+                        "");
+                  })
             ],
           ),
         ),
